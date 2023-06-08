ae9380a9948c6a1edb864413585a9a8714a6f1c0
wonj1012 <jjaa1012@gmail.com>
2023-06-08 15:56:23 +0900

    formatting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,22 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-   [owner] = await ethers.getSigners();
-   console.log(`Owner: ${owner.address}`);
-  const contractName = 'Arb';
-  await hre.run("compile");
-  const smartContract = await hre.ethers.getContractFactory(contractName);
-  const contract = await smartContract.deploy();
-  await contract.deployed();
-  console.log(`${contractName} deployed to: ${contract.address}`); 
-  console.log('Put the above contract address into the .env file under arbContract');
+    [owner] = await ethers.getSigners();
+    console.log(`Owner: ${owner.address}`);
+    const contractName = 'Arb';
+    await hre.run('compile');
+    const smartContract = await hre.ethers.getContractFactory(contractName);
+    const contract = await smartContract.deploy();
+    await contract.deployed();
+    console.log(`${contractName} deployed to: ${contract.address}`);
+    console.log(
+        'Put the above contract address into the .env file under arbContract'
+    );
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
--- scripts/gas.js
+++ scripts/gas.js
@@ -1,35 +1,34 @@
-const hre = require("hardhat");
-const fs = require("fs");
-require("dotenv").config();
+const hre = require('hardhat');
+const fs = require('fs');
+require('dotenv').config();
 
-let config,arb,owner;
+let config, arb, owner;
 const network = hre.network.name;
 if (network === 'aurora') config = require('./../config/aurora.json');
 if (network === 'fantom') config = require('./../config/fantom.json');
 
 const main = async () => {
-	[owner] = await ethers.getSigners();
-	const gasPrice = await ethers.provider.getGasPrice()
-	//{gasPrice: 1000000000001}
-	console.log(`Gas Price: ${gasPrice.toString()}`);
-	const recommendedPrice = gasPrice.mul(10).div(9);
-	console.log(`Recommended Price: ${recommendedPrice.toString()}`);
-	
-}
+    [owner] = await ethers.getSigners();
+    const gasPrice = await ethers.provider.getGasPrice();
+    //{gasPrice: 1000000000001}
+    console.log(`Gas Price: ${gasPrice.toString()}`);
+    const recommendedPrice = gasPrice.mul(10).div(9);
+    console.log(`Recommended Price: ${recommendedPrice.toString()}`);
+};
 
-process.on('uncaughtException', function(err) {
-	console.log('UnCaught Exception 83: ' + err);
-	console.error(err.stack);
-	fs.appendFile('./critical.txt', err.stack, function(){ });
+process.on('uncaughtException', function (err) {
+    console.log('UnCaught Exception 83: ' + err);
+    console.error(err.stack);
+    fs.appendFile('./critical.txt', err.stack, function () {});
 });
 
 process.on('unhandledRejection', (reason, p) => {
-	console.log('Unhandled Rejection at: '+p+' - reason: '+reason);
+    console.log('Unhandled Rejection at: ' + p + ' - reason: ' + reason);
 });
 
 main()
-	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error(error);
-		process.exit(1);
-	});
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
